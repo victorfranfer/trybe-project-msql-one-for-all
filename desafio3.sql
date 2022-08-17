3b8c0f54239a673f4e063bb39d10db0459234664
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-08-16 23:36:34 -0300

    req3:fix

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,11 @@
 SELECT
 	us.usuario_nome AS usuario,
-    COUNT(re.cancao_id) AS qt_de_musicas_ouvidas,
-    ROUND(SUM(ca.duracao_segundos / 60), 2) AS total_minutos
+  COUNT(re.usuario_id) AS qt_de_musicas_ouvidas,
+  ROUND(SUM(ca.duracao_segundos / 60), 2) AS total_minutos
 FROM SpotifyClone.usuarios us
 JOIN SpotifyClone.reproducoes re
 	ON us.usuario_id = re.usuario_id
 JOIN SpotifyClone.cancoes ca
 	ON re.cancao_id = ca.cancao_id
-GROUP BY us.usuario_nome;
+GROUP BY usuario
+ORDER BY usuario;
